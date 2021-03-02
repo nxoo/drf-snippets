83f8b619ac35ea3532b4a6dbcfa74ff03fe39c6e
nxoo <brianwangombe11@gmail.com>
2021-03-02 11:07:59 +0300

    fixed error in 'quickstart.apps.QuickstartConfig'

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'quickstart.polls.QuickstartConfig',
+    'quickstart.apps.QuickstartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
