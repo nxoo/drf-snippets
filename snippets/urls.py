9500278d4ca59251b0c3a5d395577d8510518205
nxoo <brianwangombe11@gmail.com>
2021-03-05 15:06:17 +0300

    changed snippet list and detail views from function based to class based

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail)
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
