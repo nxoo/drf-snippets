bc262fc51c3b36b31326d58a3bc50adf3ed3bc0b
nxoo <brianwangombe11@gmail.com>
2021-03-07 14:33:32 +0300

    deleted the api_root since DefaultRouter creates the API root view for us

--- snippets/views.py
+++ snippets/views.py
@@ -1,19 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions, renderers, viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format),
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
